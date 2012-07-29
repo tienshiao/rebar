8ee33d5261d1ea9584583ccd1d8030dd49eda451
Tienshiao Ma <tienshiao@tienshiao.org>
2012-07-29 16:54:33 -0700

    switch to using some jquery

--- template.js
+++ template.js
@@ -4,50 +4,45 @@ function initBar() {
             if (e.message) {
                 var link = e.message;
 
-                if (document.readyState == 'complete') {
+                $(document).ready(function() {
                     displayBar(link);
-                } else {
-                    window.addEventListener('load', function() {
-                        displayBar(link);
-                    });
-                }
+                });
             }
         } else if (e.name === 'gainFocus') {
-            document.getElementsByTagName('body')[0].className = document.getElementsByTagName('body')[0].className.replace(/(?:^|\s)inactive(?!\S)/, '');
+            $('body').removeClass('inactive');
         } else if (e.name === 'loseFocus') {
-            document.getElementsByTagName('body')[0].className += ' inactive';
+            $('body').addClass('inactive');
         }
     }, false);
     safari.self.tab.dispatchMessage('getLink', null);
 }
 
 function displayBar(link) {
-    document.getElementById('logo').setAttribute('href', 'http://' + link.reddit + '/');
-    document.getElementById('score').innerHTML = link.score;
-    document.getElementById('title').setAttribute('href', link.commentsHref);
-    document.getElementById('title').innerHTML = link.title;
-    document.getElementById('subreddit').setAttribute('href', 'http://www.reddit.com/r/' + link.subreddit);
-    document.getElementById('subreddit').innerHTML = '/r/' + link.subreddit;
-    document.getElementById('comments').setAttribute('href', link.commentsHref);
-    document.getElementById('comment-count').innerHTML = link.comments;
+    $('#logo').attr('href', 'http://' + link.reddit + '/');
+    $('#score').html(link.score);
+    $('#title').attr('href', link.commentsHref)
+               .html(link.title);
+    $('#subreddit').attr('href', 'http://' + link.reddit + '/r/' + link.subreddit)
+                   .html('/r/' + link.subreddit);
+    $('#comments').attr('href', link.commentsHref);
+    $('#comment-count').html(link.comments);
     
-    var title = document.getElementById('title');
-    safari.self.tab.dispatchMessage('setHeight', parseInt(window.getComputedStyle(title,"").getPropertyValue('height')) + 10 + 'px');
-    window.addEventListener('resize', function(e) {
-        safari.self.tab.dispatchMessage('setHeight', parseInt(window.getComputedStyle(title,"").getPropertyValue('height')) + 10 + 'px');
-    }, false);
+    safari.self.tab.dispatchMessage('setHeight', $('.bar').first().outerHeight() + 'px');
+    $(window).resize(function() {
+        safari.self.tab.dispatchMessage('setHeight', $('.bar').first().outerHeight() + 'px');
+    });
 
-    document.getElementById('close').addEventListener('click', function(e) {
+    $('#close').click(function() {
         safari.self.tab.dispatchMessage('close', null);
-    }, false);
+    });
 
-    window.addEventListener('focus', function(e) {
+    $(window).focus(function() {
         safari.self.tab.dispatchMessage('gainFocus', null);
-    }, false);
+    });
 
-    window.addEventListener('blur', function(e) {
+    $(window).blur(function() {
         safari.self.tab.dispatchMessage('loseFocus', null);
-    }, false);
+    });
 
 }
 
